ad321b8a97836e42f887b8dd3f5ec7d2026cba07
HudaElhady <huda.m.elhady@gmail.com>
2017-08-12 21:07:54 +0200

    Rounds are done

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,16 +11,39 @@ import UIKit
 class ViewController: UIViewController {
       
       //MARK: - Variables
-      var currentValue : Int = 50
-      var targetValue : Int = 0
+      var currentValue = 50
+      var targetValue = 0
+      var score = 0
+      var round = 0
+      
       
       //MARK: - Outlets
       @IBOutlet weak var slider : UISlider!
-
+      @IBOutlet weak var targetLabel : UILabel!
+      @IBOutlet weak var scoreLabel : UILabel!
+      @IBOutlet weak var roundLabel : UILabel!
+      
+      
+      //MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
-      targetValue = 1 + Int(arc4random_uniform(100))
+      let imageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+      let imageHeighlight = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+      slider.setThumbImage(imageNormal, for: .normal)
+      slider.setThumbImage(imageHeighlight , for: .highlighted)
+      
+      let trackleLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+      let trackleRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+      let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+      let trackLeftResizable = trackleLeftImage.resizableImage(withCapInsets: insets)
+      let trackRightResizable = trackleRightImage.resizableImage(withCapInsets: insets)
+      slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+      slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+      
+      
+
+        startNewGame()
+      updateLabels()
       }
     @IBAction func buttonClicked(_ sender: UIButton) {
         print("You finished!")
@@ -31,15 +54,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
       
+      
+      func startNewRound()  {
+            currentValue = 50
+            targetValue = 1 + Int(arc4random_uniform(100))
+            round += 1
+          //  slider.value = Float(currentValue)
+      }
+      func updateLabels() {
+      targetLabel.text = "\(targetValue)"
+      scoreLabel.text = "\(score)"
+      roundLabel.text = "\(round)"
+      }
+      
+      func startNewGame()  {
+            round = 0
+            score = 0
+            startNewRound()
+      }
+      
       //MARK: - Actions
+@IBAction func startOver()
+{
+      startNewGame()
 
+      updateLabels()
+}
+      
 @IBAction func showAlert()
 {
-      let message = "Slider value is : \(currentValue)" + "\n the target value is :\(targetValue)"
-    let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
-    let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+      let difference = abs(targetValue - currentValue)
+      var points = 100 - difference
+      
+      let title : String
+      if difference == 0 {
+            title = "Perfect"
+            points += 100
+      }
+      else if difference < 5
+      {
+            title = "You almost there"
+            if difference == 1 {
+                  points += 50
+            }
+      }else if difference < 10
+      {
+            title = "Pretty good"
+      }else{
+      title = "Not even good .."
+      }
+      score += points
+      let message = "your score is \(points)"
+    let alert = UIAlertController(title: title , message: message, preferredStyle: .alert)
+      let action = UIAlertAction(title: "Ok", style: .default, handler: {
+      action in
+            self.startNewRound()
+            self.updateLabels()
+      })
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
+//      startNewRound()
+//      updateLabels()
     
 }
     @IBAction func sliderMoved (_ slider : UISlider)
